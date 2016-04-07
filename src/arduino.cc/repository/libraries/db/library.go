9bee12c8b2980b32548ffc4d702fbc62ab6acede
Cristian Maglie <c.maglie@arduino.cc>
2016-04-07 14:39:33 +0200

    Pass of go fmt

--- src/arduino.cc/repository/libraries/db/library.go
+++ src/arduino.cc/repository/libraries/db/library.go
@@ -12,17 +12,17 @@ func FromLibraryToRelease(library *metadata.LibraryMetadata) *Release {
 	}
 
 	dbRelease := Release{
-		LibraryName:     library.Name,
-		Version:         VersionFromString(library.Version),
-		Author:          library.Author,
-		Maintainer:      library.Maintainer,
-		License:         library.License,
-		Sentence:        library.Sentence,
-		Paragraph:       library.Paragraph,
-		Website:         library.URL, // TODO: Rename "url" field to "website" in library.properties
-		Category:        library.Category,
-		Architectures:   architectures,
-		Types:           library.Types,
+		LibraryName:   library.Name,
+		Version:       VersionFromString(library.Version),
+		Author:        library.Author,
+		Maintainer:    library.Maintainer,
+		License:       library.License,
+		Sentence:      library.Sentence,
+		Paragraph:     library.Paragraph,
+		Website:       library.URL, // TODO: Rename "url" field to "website" in library.properties
+		Category:      library.Category,
+		Architectures: architectures,
+		Types:         library.Types,
 	}
 
 	return &dbRelease
