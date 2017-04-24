80c69a19757536b5d41a310d1899d5addc6bb0bd
Cristian Maglie <c.maglie@arduino.cc>
2017-04-24 17:45:32 +0200

    Added library extractor from issue

---
+++ extract_library_from_issue.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+ISSUE=$1
+REPO=arduino/Arduino
+
+if [ -z $ISSUE ]; then
+	echo Please specify the issue number
+	echo $0 6543
+	exit 1
+fi
+
+REPO_URL=`curl -s -f https://api.github.com/repos/$REPO/issues/$ISSUE | jq -r .body | egrep -o 'https?://[^ )]+'`.git
+
+echo Detected repo url is: $REPO_URL
+echo Press enter to continue
+read
+
+
+TMP=`mktemp -u`
+
+git clone $REPO_URL $TMP
+if [ $? != 0 ]; then
+	echo "Failed to fetch $REPO_URL."
+	exit 1
+fi
+cd $TMP
+TAGS=`git tag`
+echo TAGS=$TAGS
+
+if [ -z $TAGS ]; then
+	echo "ERROR: Failed to detect TAGS."
+	exit 2
+fi
+
+git config advice.detachedHead false
+
+for TAG in "$TAGS"; do
+	git checkout "$TAG"
+	NAME=`cat library.properties | grep name`
+	if [ $? != 0 ]; then
+		echo "Invalid library.properties in tag $TAG"
+		continue
+	fi
+	NAME=${NAME#*=}
+	echo "Found name: $NAME"
+done
+
+if [ -z $NAME ]; then
+	echo "ERROR: Failed to detect library NAME."
+	exit 2
+fi
+
+echo ""
+echo ">> Library content:"
+ls -l
+
+cd -
+
+rm -rf $TMP
+
+echo ""
+echo ">> possible duplicates on repositories.txt:"
+grep "$NAME" -i repositories.txt
+
+ADDED_LINE="$REPO_URL|Contributed|$NAME"
+
+echo ""
+echo ">> Line to be added:"
+echo $ADDED_LINE
+echo Press enter to proceed
+read
+
+echo $ADDED_LINE >> repositories.txt
+git add -u
+echo -e "Added lib $NAME\n\nhttps://github.com/arduino/Arduino/issues/$ISSUE" | git commit -F -
+
