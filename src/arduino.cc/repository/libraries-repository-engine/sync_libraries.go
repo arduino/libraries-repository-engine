67f29b821bf32ba6f483863f00824a8d6d37d856
Cristian Maglie <c.maglie@arduino.cc>
2018-01-15 16:34:56 +0100

    Temporary revert "Do not publish libraries with wrong name"
    This reverts commit ff3328cb53c82dba2b8b98dbd10b37c4c68ddfa6.

--- src/arduino.cc/repository/libraries-repository-engine/sync_libraries.go
+++ src/arduino.cc/repository/libraries-repository-engine/sync_libraries.go
@@ -234,12 +234,7 @@ func syncLibraryTaggedRelease(logger *log.Logger, repo *git.Repository, tag stri
 		return fmt.Errorf("Error generating library from repo: %s", err)
 	}
 	library.Types = repoMeta.Types
-
-	// If the release name is different from the listed name, skip release...
-	if library.Name != repoMeta.LibraryName {
-		logger.Printf("Release %s:%s has wrong library name, should be %s", library.Name, library.Version, repoMeta.LibraryName)
-		return nil
-	}
+	library.Name = repoMeta.LibraryName
 
 	// If the release is already checked in, skip
 	if libraryDb.HasLibrary(library.Name) && libraryDb.HasReleaseByNameVersion(library.Name, library.Version) {
