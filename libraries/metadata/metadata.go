0950be8dfa46aab48a69228f90a9e3644031c41e
per1234 <accounts@perglass.com>
2021-05-21 20:50:12 -0700

    Remove obsolete comments from source code
    These comments refer to code that has been removed or replaced, and so are no longer of any relevance.

--- libraries/metadata/metadata.go
+++ libraries/metadata/metadata.go
@@ -27,11 +27,6 @@ Package metadata handles library.properties metadata.
 The functions in this package helps on parsing/validation of
 library.properties metadata. All metadata are parsed into a
 LibraryMetadata structure.
-
-The source of may be any of the following:
-- a github.PullRequest
-- a github.RepositoryContent
-- a byte[]
 */
 package metadata
 
